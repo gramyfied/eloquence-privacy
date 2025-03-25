e42ac4155f9861028e1e6e5b87dfe63b2bb0acbe
Gramy <gramy@example.com>
2025-03-25 21:09:58 +0100

    Update main.dart to use App widget from app/app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart' as provider;
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'app/theme.dart';
-import 'app/router.dart';
+import 'app/app.dart';
 import 'application/services/di/service_locator.dart';
 import 'domain/repositories/auth_repository.dart';
 import 'domain/repositories/audio_repository.dart';
@@ -25,38 +24,5 @@ void main() async {
   // Initialiser les services avec injection de dépendances
   await initializeServiceLocator();
   
-  runApp(const EloquenceApp());
-}
-
-class EloquenceApp extends StatelessWidget {
-  const EloquenceApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return provider.MultiProvider(
-      providers: [
-        provider.Provider<AuthRepository>(
-          create: (_) => serviceLocator<AuthRepository>(),
-        ),
-        provider.Provider<ExerciseRepository>(
-          create: (_) => serviceLocator<ExerciseRepository>(),
-        ),
-        provider.Provider<SessionRepository>(
-          create: (_) => serviceLocator<SessionRepository>(),
-        ),
-        provider.Provider<AudioRepository>(
-          create: (_) => serviceLocator<AudioRepository>(),
-        ),
-        provider.Provider<SpeechRecognitionRepository>(
-          create: (_) => serviceLocator<SpeechRecognitionRepository>(),
-        ),
-      ],
-      child: MaterialApp.router(
-        title: 'Éloquence',
-        theme: AppTheme.theme,
-        routerConfig: AppRouter.router,
-        debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
+  runApp(const App());
 }
